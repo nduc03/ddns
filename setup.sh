3a344fd08662ffcd7f468ee49ec39035b93cbfe2
Duc <48994241+nduc03@users.noreply.github.com>
2025-01-25 15:54:24 +0700

    change to system service

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
+echo "Ask for sudo password..."
+sudo echo "Done."
 cd ~
 mkdir ddns
-mkdir -p ~/.config/systemd/user
 cd ddns
 git clone https://github.com/nduc03/ddns.git .
 # on host please run this (at project directory) after clone: scp .env nduc@server.home:~/ddns/.env
 echo "On host please run this ssh command (at project directory) after clone: scp .env nduc@server.home:~/ddns/.env."
 read -p "Press enter to continue after .env file is copied"
-pip install -r requirements.txt
-cp ddns.service ~/.config/systemd/user/ddns.service
-systemctl --user daemon-reload
-systemctl --user enable ddns.service
+sudo pip install -r requirements.txt
+sudo cp ddns.service etc/systemd/system/ddns.service
+sudo systemctl daemon-reload
+sudo systemctl --user enable ddns.service
